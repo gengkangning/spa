cb8f6139bcf1fe8f1b5f9ed6d506c7ad08cac324
gengkangning <948891770@qq.com>
2018-03-07 15:10:23 +0800

    1

--- week1/calculate/demo1.js
+++ week1/calculate/demo1.js
@@ -1,13 +1,11 @@
-var width=document.getElementsByClassName('rectangle_width');
-var height=document.getElementsByClassName('rectangle_height');
-var calc=document.getElementsByClassName('rectangle_calc');
-var perimeter=document.getElementsByClassName('rectangle_perimeter');
-var area=document.getElementsByClassName('rectangle_area');
 
-calc.onclick=function(){
-  console.log('1');
-  console.log(width.value);
-  perimeter.value=2*width.value+2*height.value;
-  area.value=width.value*height.value;
+var cal=function(){
+	var width=document.getElementById('rectangle_width');
+	var height=document.getElementById('rectangle_height');
+	var calc=document.getElementById('rectangle_calc');
+	var perimeter=document.getElementById('rectangle_perimeter');
+	var area=document.getElementById('rectangle_area');   
+	perimeter.value=2*width.value+2*height.value;
+	area.value=width.value*height.value;
 };
 
